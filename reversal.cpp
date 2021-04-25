6552082502697b88318c62cf9488cc92d500cc51
AkjDragonMaster <76256480+AkjDragonMaster@users.noreply.github.com>
2021-04-25 19:42:27 +0530

    reversal of number 2
    again

--- reversal.cpp
+++ reversal.cpp
@@ -1,15 +1,16 @@
-#include<stdio.h>  
- int main()    
+#include<stdio.h>
+int main()    
 {    
-int num, reversal=0, i;    
-printf("Enter a number: ");    
-  scanf("%d", &num);    
-  while(num!=0)    
-  {    
-     i=num%10;    
-     reversal=reversal*10+i;    
-     num/=10;    
-  }    
-  printf("Reversed Number: %d",reversal);    
+    int num,reversal=0,i,copy;    
+    printf("Enter a number: ");    
+    scanf("%d", &num);    
+    copy = num;
+    while(num!=0)    
+    {    
+        i=num%10;    
+        reversal=reversal*10+i;    
+        num/=10;    
+    }    
+    printf("Reversal of %d is %d",copy,reversal);    
 return 0;  
 }   

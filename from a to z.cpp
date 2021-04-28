af7e70c35cb63c8fab09e095a99fb2eca027737b
AkjDragonMaster <76256480+AkjDragonMaster@users.noreply.github.com>
2021-04-28 17:38:21 +0530

    Sir I did it from a to z
    But I had to use two loops for for not making all the other graphical characters in the final output. Please show the working using A to z with out using two loops

---
+++ from a to z.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    for(char alpha = 97;alpha <= 122;alpha++){
+        printf("%c \n", alpha);
+			}
+	for(char ALPHA=65; ALPHA <= 90; ALPHA++){
+	    printf("%c \n", ALPHA);
+	}
+    
+    return 0;
+}

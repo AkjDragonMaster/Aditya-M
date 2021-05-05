6e64b38d7d1875a23f07f777e7d9cbd6d051f292
AkjDragonMaster <76256480+AkjDragonMaster@users.noreply.github.com>
2021-05-05 12:37:03 +0530

    Add files via upload

--- half_way rverse array.cpp
+++ half_way rverse array.cpp
@@ -6,13 +6,16 @@ int main()
     printf("Enter the size of array: ");
     scanf("%d", &size);
     int array[size];
-    for(int i=0;i < size; i++){
+    for(int i=1;i < size + 1; i++){
         printf("Enter value of number %d: ", i);
         scanf("%d", &array[i]);
     }
-    for(int a = size - 1; a >= 0; a -= 1){
-        printf("The result of array %d is %d; \n",a,array[a]);
+    for(int a = size/2; a != 0; a -= 1){
+        printf("The value of element %d is %d \n",a,array[a]);
     }
-
+    for(int a = size/2 + 1; a < size + 1; a++){
+        printf("The value of element %d is %d \n",a,array[a]);
+    }
+    
     return 0;
 }

37e7c88f54a3a633044671873d5fc49547df367f
AkjDragonMaster <76256480+AkjDragonMaster@users.noreply.github.com>
2021-10-10 11:59:49 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,5 @@
+public class assignment {
+    public static void main(String[] args) {
+       System.out.println("Fathers name - Subho Mondal \nMothers name - Pratima Banerjee \nMy name - Aditya Mondal."); 
+    }
+}
